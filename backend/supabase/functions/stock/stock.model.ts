8c9abf0ec3023f7531157f3fb075dc1648c41588
AvocadoAguacate <estebanguzra@gmail.com>
2024-11-01 23:02:29 -0600

    UDATE interface

--- backend/supabase/functions/stock/stock.model.ts
+++ backend/supabase/functions/stock/stock.model.ts
@@ -1,8 +1,8 @@
 export interface PokemonProduct {
-  p_pokemon_id: number;   // ID del Pokémon
-  p_gender_id: number;    // ID del género
-  p_generation: number;   // Generación del producto
-  p_base_cost: number;    // Costo base del producto
+  p_pokemon_id: number;
+  p_gender_id: number;
+  p_generation: number;
+  p_base_cost: number;
   p_salt: string;
 }
 
