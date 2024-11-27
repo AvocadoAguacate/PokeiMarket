fba57029e7ffb388eb978d1b58b184f4c1099831
AvocadoAguacate <estebanguzra@gmail.com>
2024-11-26 21:48:10 -0600

    ADD config form

--- frontend/src/app/login/components/login/login.component.ts
+++ frontend/src/app/login/components/login/login.component.ts
@@ -1,12 +1,29 @@
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  public LoginForm = new FormGroup({
+    email: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl<string>('', {nonNullable: true}) 
+  });
 }
