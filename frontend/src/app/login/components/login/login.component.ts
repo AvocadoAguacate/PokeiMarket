156fedd8946c1b012d8f13def02a2527798a91e2
AvocadoAguacate <estebanguzra@gmail.com>
2024-11-27 22:57:46 -0600

    ADD login auth

--- frontend/src/app/login/components/login/login.component.ts
+++ frontend/src/app/login/components/login/login.component.ts
@@ -1,8 +1,11 @@
+import { AuthService } from './../../../shared/services/auth.service';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,18 +15,47 @@ import { CommonModule } from '@angular/common';
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
   public LoginForm = new FormGroup({
     email: new FormControl<string>('', {
       nonNullable: true,
       validators: [Validators.required, Validators.email],
     }),
     password: new FormControl<string>('', {nonNullable: true}) 
-  });
+  }); 
+  constructor(
+    private authService:AuthService,
+    private router:Router
+  ){
+
+  }
+
+  onRegister() {
+    this.router.navigate(['/auth/register'])
+  }
+  onSubmit() {
+    if(this.LoginForm.invalid) return;
+    this.authService.signIn(this.LoginForm.value.email!, this.LoginForm.value.password!)
+    .then(({data, error}) => {
+      if(error){ //cuando logra conectarse al backend
+        console.log(error);
+      } else {
+        console.log(data);
+        if(data.user?.user_metadata?.['role'] === 'admin'){
+          this.router.navigate(['/admin']);
+        } else {
+          this.router.navigate(['/store']);
+        }
+      }
+    }).catch((err) => { //no logra conectarse al backend
+      console.log(err);
+    })
+  }
+
 }
--- frontend/src/app/shared/services/auth.service.ts
+++ frontend/src/app/shared/services/auth.service.ts
@@ -1,9 +1,58 @@
+// import { AuthResponse } from './../../../../node_modules/@supabase/auth-js/src/lib/types';
 import { Injectable } from '@angular/core';
-
+import { createClient, Session, AuthResponse } from '@supabase/supabase-js';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private supabaseUrl = 'http://127.0.0.1:54321';
+  private supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0';
+  private supabase = createClient(this.supabaseUrl, this.supabaseKey);
+  constructor() {
+    this.initializeAuthListener();
+  }
 
-  constructor() { }
+  async getSession(): Promise<Session | null> {
+    const { data } = await this.supabase.auth.getSession();
+    return data?.session || null;
+  }
+
+  async refreshSession(): Promise<void> {
+    const { error } = await this.supabase.auth.refreshSession();
+    if (error) {
+      console.error('Error al renovar la sesión:', error.message);
+    }
+  }
+
+  async signIn(email: string, password: string):Promise<AuthResponse>{
+    return this.supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+  }
+
+  async register(email: string, password: string):Promise<AuthResponse>{
+    return this.supabase.auth.signUp({
+      email,
+      password,
+      options:{
+        data: {
+          role: 'client'
+        }
+      }
+    });
+  }
+  // Escuchar eventos de autenticación
+  initializeAuthListener(): void {
+    this.supabase.auth.onAuthStateChange((event, session) => {
+      console.log('Evento de autenticación:', event);
+      if (event === 'SIGNED_IN') {
+        console.log('Usuario inició sesión:', session);
+      } else if (event === 'SIGNED_OUT') {
+        console.log('Usuario cerró sesión');
+      } else if (event === 'TOKEN_REFRESHED') {
+        console.log('Token renovado:', session?.access_token);
+      }
+    });
+  }
 }
